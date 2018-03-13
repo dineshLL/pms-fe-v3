3c7702478eb5fc038c491c0d1bc797f1d82c63e1
dineshLL <dineshgitt@gmail.com>
2018-03-13 14:07:23 +0530

    feat(gen55) questions development completed

--- src/app/common/common-forms/grnff-questions/grnff-questions.component.ts
+++ src/app/common/common-forms/grnff-questions/grnff-questions.component.ts
@@ -21,12 +21,23 @@ export class GrnffQuestionsComponent implements OnInit {
       amountFromPension: [''],
       deductFromGratuity: [false, Validators.required],
       amountOrPercentage: ['Amount'],
-      amountFromGratuity: ['']
+      amountFromGratuity: [''],
+      pensionStartingDate: ['', Validators.required],
+      serviceBreaks: [false, Validators.required],
+      tempPensionReceived: [false, Validators.required],
+      pensionReceived: [false, Validators.required],
+      releasedForTempService: [false, Validators.required],
+      joinedForFunds: [false, Validators.required],
+      contributionRefunded: [false]
     });
   }
 
   get deductFromGratuity() { return this.form.get('deductFromGratuity')}
   get deductFromPension() { return this.form.get('deductFromPension')}
   get amountOrPercentage() { return this.form.get('amountOrPercentage')}
+  get serviceBreaks() { return this.form.get('serviceBreaks') }
+  get tempPensionReceived() { return this.form.get('tempPensionReceived') }
+  get releasedForTempService() { return this.form.get('releasedForTempService') }
+  get joinedForFunds() { return this.form.get('joinedForFunds') }
 
 }
